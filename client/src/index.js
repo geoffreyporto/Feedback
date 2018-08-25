54669b818657790460e4eb9710a39978b728c532
Marlon Decosta <marlondecosta@Embodied-Mac-Pro-Beta.local>
2018-08-25 00:40:11 -0400

    survey form and email

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,10 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 import reducers from './reducers' 
 
+// Development only axios helpers
+import axios from 'axios'
+window.axios = axios 
+
 const store = createStore(reducers, {}, applyMiddleware(Thunk))
 
 ReactDOM.render(
